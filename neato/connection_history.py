829c2bcbc85d1129702e3d37770a2851cf3a38ce
MohamedElMistiri <66651404+MohamedElMistiri@users.noreply.github.com>
2022-04-25 12:48:19 -0700

    Added allnodes into connection history
    allnodes in connection history to keep tack of node layers

--- neato/connection_history.py
+++ neato/connection_history.py
@@ -10,9 +10,16 @@ class ConnectionHistory(object):
         self.allConnections: List[Connection] = []
         self.global_innovation_count = 0
         self.hidden_layers = hidden_layers
+        self.allNodes: List[Node] = []
 
     def exists(self, n1: Node, n2: Node):
         for c in self.allConnections:
             if c.in_node.number == n1.number and c.out_node.number == n2.number:
                 return c
+        return None
+
+    def node_exists(self,n1: int):
+        for node in self.allNodes:
+            if node.number == n1:
+                return node
         return None

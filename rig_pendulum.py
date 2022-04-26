9e9433e3d6fba742b906bb7e6dc11650d8d98b2c
Soham <sohamsahare@gmail.com>
2022-04-26 02:14:41 -0700

    moved files into folders

---
+++ rig_pendulum.py
@@ -0,0 +1,26 @@
+# %%
+from neato.brain import Brain
+from neato.species import Species
+import pickle
+# %%
+brain = Brain.load('pendulum')
+with open('pendulum_best_individual_gen88', 'rb') as f:
+        genome = pickle.load(f)
+# %%
+brain._species = [
+    Species(
+        brain._hyperparams.max_fitness_history, genome
+    )
+]
+for s in brain._species:
+    while len(s._members) < 200:
+        m = genome.clone()
+        m.mutate(brain._hyperparams.mutation_probabilities)
+        s._members.append(m)
+# %%
+for s in brain._species:
+    print(len(s._members))
+
+# %%
+brain.save('pendulum_rigged')
+# %%

a56b82833d60a1e691eb9a73cce3a2aff852fd09
Olivia Christie <olivia.christie@astrobotic.com>
2022-04-26 17:50:46 -0700

    create gif of output

--- cartpoleTest.py
+++ cartpoleTest.py
@@ -3,10 +3,25 @@ import gym
 import sys
 sys.path.append('./neato')
 from neato.genome import Genome
+from matplotlib import animation
+import matplotlib.pyplot as plt
 
+def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
+
+    #Mess with this to change frame size
+    plt.figure(figsize=(frames[0].shape[1] / 72.0, frames[0].shape[0] / 72.0), dpi=72)
+
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
+    anim.save(path + filename, writer='imagemagick', fps=60)
 
 def run():
-    with open('cartpole/neato_cartpole_best_individual', 'rb') as f:
+    with open('cartpole/neato_cartpole_best_individual_gen2', 'rb') as f:
         genome: Genome = pickle.load(f)
 
     print('Loaded genome:')
@@ -15,13 +30,16 @@ def run():
 
     env = gym.make("CartPole-v1") # BipedalWalker-v3
     observation = env.reset()
-
+    frames = []
     done = False
     while not done:
+        frames.append(env.render(mode="rgb_array"))
         action = genome.forward(observation)[0]
 
         observation, reward, done, info = env.step(action <= 0.5)
         env.render()
+    env.close()
+    save_frames_as_gif(frames)
 
 if __name__ == '__main__':
     run()
